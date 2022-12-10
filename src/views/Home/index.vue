2b1b38fd93a9c5b8724458888504fd5be8c3984c
Md. Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2022-12-10 11:09:21 +0600

    hero section and navbar color chanhging

---
+++ src/views/Home/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <Hero />
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, fugiat. Enim
+        magni veritatis quasi ipsa facilis? Cumque beatae cum nulla repellat alias qui
+        eveniet perspiciatis incidunt quis totam optio, odio expedita temporibus iusto
+        delectus. Placeat distinctio ea debitis. Ad placeat qui expedita rem nemo enim,
+        sequi reiciendis quaerat praesentium rerum!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, fugiat. Enim
+        magni veritatis quasi ipsa facilis? Cumque beatae cum nulla repellat alias qui
+        eveniet perspiciatis incidunt quis totam optio, odio expedita temporibus iusto
+        delectus. Placeat distinctio ea debitis. Ad placeat qui expedita rem nemo enim,
+        sequi reiciendis quaerat praesentium rerum!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, fugiat. Enim
+        magni veritatis quasi ipsa facilis? Cumque beatae cum nulla repellat alias qui
+        eveniet perspiciatis incidunt quis totam optio, odio expedita temporibus iusto
+        delectus. Placeat distinctio ea debitis. Ad placeat qui expedita rem nemo enim,
+        sequi reiciendis quaerat praesentium rerum!
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Hero from "./Hero.vue";
+export default {
+  name: "Home",
+  components: {
+    Hero,
+  },
+};
+</script>
