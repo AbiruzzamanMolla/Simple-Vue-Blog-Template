dfebd6a9817aed20b6f3e46c41d99465724cf38d
Md. Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2022-12-11 12:26:09 +0600

    📱 about page design

---
+++ src/views/About/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="relative z-0 h-40 sm:h-64">
+    <img src="/images/image-2.jpg" alt="about" class="h-full w-full object-cover" />
+    <div class="absolute inset-0 bg-black opacity-50"></div>
+    <div class="absolute inset-0 bg-gradient-to-t from-primary opacity-50"></div>
+    <div class="absolute inset-0">
+      <div
+        class="container mx-auto px-4 sm:px-0 text-white h-full w-full flex flex-col items-center justify-center sm:justify-end text-2xl sm:text-5xl"
+      >
+        <div class="sm:mb-8 text-center">About Us</div>
+      </div>
+    </div>
+  </div>
+  <div class="container mx-auto px-4 sm:px-8 py-8 sm:py-12">
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut voluptatibus quibusdam
+      qui doloremque odio! Quaerat sequi accusamus et, quo autem reprehenderit ex dolores,
+      quam, fuga veritatis eius iure impedit voluptate.
+    </p>
+    <p class="pt-4">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut voluptatibus quibusdam
+      qui doloremque odio! Quaerat sequi accusamus et, quo autem reprehenderit ex dolores,
+      quam, fuga veritatis eius iure impedit voluptate.
+    </p>
+    <p class="pt-4">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut voluptatibus quibusdam
+      qui doloremque odio! Quaerat sequi accusamus et, quo autem reprehenderit ex dolores,
+      quam, fuga veritatis eius iure impedit voluptate.
+    </p>
+    <p class="pt-4">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut voluptatibus quibusdam
+      qui doloremque odio! Quaerat sequi accusamus et, quo autem reprehenderit ex dolores,
+      quam, fuga veritatis eius iure impedit voluptate.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {},
+};
+</script>
